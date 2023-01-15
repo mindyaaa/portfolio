8061e7d2d8faf8e60523857bde0165f013aa6f94
mindyaaa <107449479+mindyaaa@users.noreply.github.com>
2023-01-15 20:21:07 +0900

    JS.scroll to menu

--- main.js
+++ main.js
@@ -17,3 +17,36 @@ function scrollHandler () {
 }
 
 document.addEventListener('scroll', scrollHandler);
+
+
+// navbar 메뉴 클릭시 해당 메뉴로 이동
+const navbarMenu = document.querySelector('.navbar__menu');
+
+function clickHandler (event) {
+    const target = event.target;
+    target.classList.toggle('active');
+
+    const link = target.dataset.link;
+    // console.log(link);
+
+    if (link == null) {
+        return;
+    }
+
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: "smooth", block: "nearest"});
+    
+
+}
+
+navbarMenu.addEventListener('click', clickHandler);
+
+// Contact버튼 클릭시 해당 영역 이동
+const contactMe = document.querySelector('.home__contact');
+
+function contactMeHandler () {
+    const scrollToContact = document.querySelector("#contact")
+    scrollToContact.scrollIntoView({behavior:"smooth"});
+}
+
+contactMe.addEventListener('click', contactMeHandler);
